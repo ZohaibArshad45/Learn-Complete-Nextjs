bee77409ba1fbc621617fbbb1ab353aba354d15b
Zohaib <zohaibfta@gmail.com>
2024-07-30 10:48:29 +0500

    Script Component

---
+++ ch1/public/ForScriptComponent.js
@@ -0,0 +1,10 @@
+ function kuchbhi (){
+    console.log('kuch bhi likh lo')
+ }
+ kuchbhi()
+
+// ---------------------------
+const kuchbhi2 = ()=>{
+    console.log('hsecond run ')
+ }
+ kuchbhi2()
--- ch1/src/app/LearnScriptComponents/page.js
+++ ch1/src/app/LearnScriptComponents/page.js
@@ -1,11 +1,23 @@
+'use client'
+import Script from 'next/script'
+
 import React from 'react'
 
 const ScriptComponent = () => {
   return (
-    <>
-    <h1> Learn Script Components</h1>
-      
-    </>
+    <main className='flex flex-col items-center mt-20 justify-center'>
+      <h1> Learn Script Components</h1>
+      <p> This is use for extra library / package </p>
+      <p> custom script code.</p>
+      <p>Show in console | For this we need to make file in public folder </p>
+
+      {/* make file in public folder */}
+      <Script src='/ForScriptComponent.js'/>
+
+       {/* we can also use onload function */}
+      <Script src='/ForScriptComponent.js' onLoad={() => { console.log('kia hall ha') }} />
+
+    </main>
   )
 }
 
