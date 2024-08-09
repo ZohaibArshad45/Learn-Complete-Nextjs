229d19de922d08cad87db5119273fd07370e7509
Zohaib <zohaibfta@gmail.com>
2024-08-09 11:16:40 +0500

    GET method

--- ch2/src/app/page.js
+++ ch2/src/app/page.js
@@ -12,8 +12,8 @@ export default function Home() {
     <main className=" flex flex-col justify-center items-center mt-10">
       <h1>WellCome Zohiab, This is <span className="text-red-800 font-bold mb-5">Home Page</span> </h1>
       <button className="bg-pink-700 p-1 pl-4 pr-4 rounded-lg mt-2 hover:bg-yellow-500" onClick={firstAPI}>What is API, About API</button>
-      <button className="bg-pink-700 p-1 pl-4 pr-4 rounded-lg mt-2 hover:bg-yellow-500" onClick={()=> router.push('/api/employee')}>Own API, GET</button>
-      <button className="bg-pink-700 p-1 pl-4 pr-4 rounded-lg mt-2 hover:bg-yellow-500" onClick={()=> router.push('/api/learn-post-method')}>Postman, POST MEethod</button>
+      <button className="bg-pink-700 p-1 pl-4 pr-4 rounded-lg mt-2 hover:bg-yellow-500" onClick={()=> router.push('/api/learn-GET-method')}>Own API, GET method</button>
+      <button className="bg-pink-700 p-1 pl-4 pr-4 rounded-lg mt-2 hover:bg-yellow-500" onClick={()=> router.push('/api/learn-post-method')}>Postman, POST method</button>
 
     </main>
   );
