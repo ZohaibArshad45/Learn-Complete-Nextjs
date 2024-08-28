8d48548ab3ac276d430022043bd92d9689212288
Zohaib <zohaibfta@gmail.com>
2024-08-28 14:30:18 +0500

    First Program Server Side

--- ch3/src/app/api/first/route.js
+++ ch3/src/app/api/first/route.js
@@ -1,4 +1,11 @@
-export async function GET(req) {
-    return new Response('Hello Zohaib')
-    
+// export async function GET(req) {
+//     return new Response('Hello Zohaib hhh')
+// }
+
+import { NextResponse } from "next/server";
+
+export const GET = (req) => {
+    return NextResponse.json({ name: "ZOhaib", 
+        age: 23, 
+        clg: "IUB" });
 }
--- ch3/src/app/page.js
+++ ch3/src/app/page.js
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <></>
-    <></>
+    <h1>Well Come Zohaib</h1>
+    
     </>
   );
 }
